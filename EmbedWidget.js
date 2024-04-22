5af2f363b2c18a8991df4a503890b24e85064aca
erik-sveen <167814675+erik-sveen@users.noreply.github.com>
2024-04-22 22:18:41 +0200

    Update EmbedWidget.js

--- EmbedWidget.js
+++ EmbedWidget.js
@@ -1,14 +1,18 @@
 (function() {
     var EmbedWidget = {
         init: function(options) {
-            var iframe = document.createElement('iframe');
-            iframe.src = options.url;
-            iframe.width = options.width || '100%';
-            iframe.height = options.height || '400px';
-            iframe.frameBorder = '0';
-            iframe.allow = 'autoplay; encrypted-media';
-            iframe.allowFullscreen = true;
-            document.getElementById(options.containerId).appendChild(iframe);
+            var container = document.getElementById(options.containerId);
+            var button = document.createElement('button');
+            button.innerText = options.buttonText || 'Open Embed';
+            button.onclick = function() {
+                var iframe = document.createElement('iframe');
+                iframe.src = options.url;
+                iframe.width = '100%';
+                iframe.height = '600px'; // Set height or make it configurable
+                iframe.style.border = '0';
+                container.appendChild(iframe);
+            };
+            container.appendChild(button);
         }
     };
 
